46a22985b3def8add09ae4dab560c2850348c708
Eliziel-Camperos <a.22308051281159@cbtis128.edu.mx>
2024-10-14 10:08:22 -0600

    Delete Tallerv2-5JCamperos directory

--- Tallerv2-5JCamperos/cbtis_app/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Alumno(models.Model):
-	apaterno =models.Charfield(max_length=50,verbose_name='Apaterno')
--- Tallerv2-5JCamperos/cbtis_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from cbtis_app import urls
-# Create your views here.
-def index(request):
-    return render(request,"index.html")
